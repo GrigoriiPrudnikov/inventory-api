c4e6a8dc429b57c41927b07a8367f521d6c9b0b4
Grigorii Prudnikov <griprudnikov742@gmail.com>
2025-06-01 14:03:50 +0500

    refactor: move handlers to handlers/user.go and handlers/item.go

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"inventory-api/internal/api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRoutes() {
 	r := gin.Default()
@@ -9,18 +13,17 @@ func SetupRoutes() {
 		c.JSON(200, "Hello World!")
 	})
 
-	r.GET("/users", GetUsers)
-	r.GET("/users/:id", GetUser)
-	r.POST("/users", CreateUser)
+	r.GET("/users", handlers.GetUsers)
+	r.GET("/users/:id", handlers.GetUser)
+	r.POST("/users", handlers.CreateUser)
 
-	r.GET("/items", GetItems)
-	r.GET("/items/:id", GetItem)
+	r.GET("/items", handlers.GetItems)
+	r.GET("/items/:id", handlers.GetItem)
 
 	r.Run(":8080")
 }
 
 /*
-POST /user - create user
 POST /item - create item and add it to user (jwt)
 POST /items/:id/sell - sell item
 */
